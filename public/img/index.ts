24ecdf53fbd1ae5c1a9c026e8d1d38ad4404d97c
LAPTEVA Sophia 00061052 <sophia.lapteva@3ds.com>
2022-11-25 23:02:47 +0100

    done

---
+++ public/img/index.ts
@@ -0,0 +1,9 @@
+import sofia from "./sofia.jpg";
+import igor from "./igor.jpg";
+import vitya from "./vitya.jpg";
+import irina from "./irina.jpg";
+import grisa from "./grisa.jpg";
+import vera from "./vera.jpg";
+import nick from "./nick.jpg";
+
+export { sofia, igor, grisa, vitya, irina, vera, nick };
